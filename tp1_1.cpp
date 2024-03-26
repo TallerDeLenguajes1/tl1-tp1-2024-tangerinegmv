00971f44f851caf49565b636a791025e07ce5e82
“Victoria”~ <gimenezmariavictoria95@gmail.com>
2024-03-26 13:36:23 -0300

    prueba 2

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -10,7 +10,7 @@ int main(){
     printf("la dirección de memoria de la variable.: %d \n", &a);
     printf("La dirección de memoria almacenada por el puntero: %d \n", &punteroA);
     printf("el tamaño de memoria utilizado por esa variable usando la función sizeof(): %d \n", sizeof(a));
-    printf("prueba commit");
-    
+    //printf("prueba commit");
+
     return 0;
 }
