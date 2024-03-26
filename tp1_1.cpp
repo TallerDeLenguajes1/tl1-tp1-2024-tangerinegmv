d62fe3eaf258c668b71971e840f7496aed1e3387
“Victoria”~ <gimenezmariavictoria95@gmail.com>
2024-03-26 13:12:16 -0300

    resolvi el punto 3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,14 @@
 
 int main(){
     printf("Hola mundo");
+    int a = 26;
+    int *punteroA;
+    punteroA = &a;
+    printf("Contenido del puntero: %d \n", *punteroA);
+    printf("La dirección de memoria almacenada por el puntero: %d \n", punteroA);
+    printf("la dirección de memoria de la variable.: %d \n", &a);
+    printf("La dirección de memoria almacenada por el puntero: %d \n", &punteroA);
+    printf("el tamaño de memoria utilizado por esa variable usando la función sizeof(): %d \n", sizeof(a));
 
     return 0;
 }
